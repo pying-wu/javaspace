f2744a9a1f5de2b2b14a4dc843f08613d4bf61e8
pying-wu <sunnyright159@gmail.com>
2021-05-13 14:15:26 +0800

    extend

--- JavaLab/src/extend/Notebook.java
+++ JavaLab/src/extend/Notebook.java
@@ -1,23 +1,27 @@
 package extend;
-
+//ctrl+1: fix
 public class Notebook extends Product{
-    //有繼承
+	//有繼承
+    
 //	public String name;
 //	public int price;
 	public int warranty;
 	
 	public Notebook(String name, int price, int warranty) {
-		this.name = name; //如父類別封裝起來,無法直接繼承
+		//super();//當沒指令時，compiler會自動加一個沒參數的建構類別在這裡
+		super(name,price); //用super呼叫 來取代下面兩行
+		//this.name = name; //如父類別封裝起來,無法直接繼承
+		//this.price = price;
 		//setName(name);
-		this.price = price;
-		setPrice(price);
-		
+		//setPrice(price);
 		this.warranty = warranty;
 	}
     //annotation 註記
 	@Override
 	public String desc() {	
-		return String.format("名稱:%s,價錢:%d,保固:%d天", name,price,warranty);
+		String info = super.desc();//呼叫父類別的 名稱:%s,價錢:%d
+		
+		return String.format("%s,保固:%d天呦!", info ,warranty);
 	}
 	
 	
--- JavaLab/src/extend/Product.java
+++ JavaLab/src/extend/Product.java
@@ -1,18 +1,28 @@
 package extend;
 
-public class Product {
+public abstract class Product {
+	//必須要是抽象類別 才能使用抽象方法  有抽象方法必為抽象類別
+	//抽象類別不一定會有抽象方法
+	//public abstract void calc();
 
-	protected String name; //public,private,protected, 空
-	protected int price;
+	public static final double TAX;
+	static {
+		TAX = 0.05;
+		//System.out.println("static black");
+	}
+	
+	String name; //public,private,protected, 空
+	 int price;
 	
 	public Product(String name, int price) {
 		this.name = name;
 		this.price = price;
 	}
 
-	public Product() {
+	/*public Product() {
 	}
-
+     */ //都沒寫時 系統會送一個空的
+	
 	public String getName() {
 		return name;
 	}
@@ -32,5 +42,8 @@ public class Product {
 	public String desc() {
 		return String.format("名稱:%s,價錢:%d", name,price);
 	}
+	
+		
+	
 	
 }
